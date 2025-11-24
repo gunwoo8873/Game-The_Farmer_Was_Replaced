2ac161b86644efef99c914b09e870decb7393826
ChoiGunWoo <gunwoo8873@outlook.kr>
2025-11-24 09:53:44 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,51 +2,88 @@ from __builtins__ import *
 
 #########################################
 # Current Ground Size : 4 * 4
+# Items DataType : Float <0.0>
 #########################################
 
-# Items <Entities.SeedName>
-Bush = Entities.Bush
-Carrot = Entities.Carrot
-SeedTypes = [Bush, Carrot]
+# Items <Entities.Name>
+Items_Values = [Items.Hay, Items.Carrot, Items.Wood, Items.Water]
+
+# Entities <Entities.Name>
+Entities_Types = [Entities.Grass, Entities.Bush, Entities.Carrot]
+
+# Grounds <Grounds.Name>
+Grounds_Types = [Grounds.Grassland, Grounds.Soil]
 
 # Directions <0=N, 1=E, 2=S, 3=W>
 Directions = [North, East, South, West]
 
-# Functions
-def move_direction(dir):
+# Configs
+def Location_Range(l):
+    return range(l)
+
+Max_Location = get_world_size()
+
+# Actions
+def Move_Direction(dir):
     return move(dir)
 
-def range_check(v):
-    return range(v)
+def Move_Locations(x, y):
+    return get_pos_x(x), get_pos_y(y)
 
-def Locaition_check(x, y):
-    return get_pos_x() == x and get_pos_y() == y
+def Grass_Harvest():
+    if get_ground_type() == Grounds_Types[0]:
+        if can_harvest():
+            harvest()
+    elif get_ground_type() != Grounds_Types[0]:
+        till()
+        # plant(Entities_Types[0])
+        if can_harvest():
+            harvest()
 
-def Bush_Tared():
-    return plant(SeedTypes[0])
+def Bush_Harvest():
+    if get_entity_type() == Entities_Types[1]:
+        if can_harvest():
+            harvest()
+    elif get_entity_type() != Entities_Types[1]:
+        plant(Entities_Types[1])
+        if can_harvest():
+            harvest()
 
-def Carrot_Tared():
-    return till(), plant(SeedTypes[1])
+def Carrot_Harvest():
+    if get_ground_type() == Grounds_Types[1]:
+        plant(Entities_Types[2])
+        if can_harvest():
+            harvest()
+    elif get_ground_type() != Grounds_Types[1]:
+        till()
+        plant(Entities_Types[2])
+        if can_harvest():
+            harvest()
 
-# Loop Logic
+# Loop Logics
 while True:
-    for _ in range_check(4):
-        move_direction(Directions[0])
-        harvest()
-    move_direction(Directions[1])
+    for _ in Location_Range(Max_Location):
+        Grass_Harvest()
+        Move_Direction(Directions[0])
+    Move_Direction(Directions[1])
 
-    for _ in range_check(4):
-        move_direction(Directions[0])
-        Bush_Tared()
-        
-        if can_harvest():
-            harvest()
-    move_direction(Directions[1])
+    for _ in Location_Range(Max_Location):
+        Bush_Harvest()
+        Move_Direction(Directions[0])
+        # plant(Entities_Types[0])
+        # if can_harvest():
+        #     harvest()
+        # elif get_entity_type() != Entities_Types[0]:
+        #     plant(Entities_Types[0])
+    Move_Direction(Directions[1])
 
-    for _ in range_check(4):
-        move_direction(Directions[0])
-        Carrot_Tared()
-        
-        if can_harvest():
-            harvest()
-    move_direction(Directions[1])
+    for _ in Location_Range(Max_Location):
+        Carrot_Harvest()
+        Move_Direction(Directions[0])
+        # if get_entity_type() != Entities_Types[1]:
+        #     till()
+        #     plant(Entities_Types[1])
+        # elif get_entity_type() == Entities_Types[1]:
+        #     if can_harvest():
+        #         harvest()
+    Move_Direction(Directions[1])
