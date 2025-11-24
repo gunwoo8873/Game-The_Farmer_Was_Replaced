cf757babd271d463478651dc8ce3fb4a5b47d94e
ChoiGunWoo <gunwoo8873@outlook.kr>
2025-11-24 18:43:41 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,81 +1,114 @@
 from __builtins__ import *
 
 #########################################
-# Current Ground Size : 4 * 4
+# Current Ground Size : 6 * 6
 # Items DataType : Float <0.0>
 #########################################
 
-# Items <Entities.Name>
-Items_Values = [Items.Hay, Items.Carrot, Items.Wood, Items.Water]
+# Current haved item list
+Item_Values = [
+	Items.Hay,
+	Items.Carrot, 
+	Items.Wood, 
+	Items.Water
+	]
 
-# Entities <Entities.Name>
-Entities_Types = [Entities.Grass, Entities.Bush, Entities.Carrot]
+# Current Unlock Entitie List
+Entitie_Types = [
+	Entities.Grass, 
+	Entities.Bush, 
+	Entities.Carrot
+	]
 
-# Grounds <Grounds.Name>
-Grounds_Types = [Grounds.Grassland, Grounds.Soil]
+# Current Unlock Ground List
+Ground_Types = [
+	Grounds.Grassland, # The Default use of standard Seed Ground
+	Grounds.Soil # Add a new Type of Carrot Seed Ground
+	]
 
 # Directions <0=N, 1=E, 2=S, 3=W>
-Directions = [North, East, South, West]
+Directions = [
+	North, 
+	East, 
+	South, 
+	West,
+	]
 
 # Location Size
-Max_Location = get_world_size
-get_location_x = get_pos_x
-get_location_y = get_pos_y
+Max_Count = get_world_size()
+
+# Default Configs
+Default_Count = 0
+Default_Harvest = False
+
 
 # Configs
-def Location_Range(l):
-	return range(l)
+def Location_Range(i):
+	return range(i)
 
 # Actions
 def Move_Direction(dir):
 	return move(dir)
 
-def Move_Locations(x, y):
-	return get_location_x(x), get_location_y(y)
+def Ground_Type():
+	for _ in Location_Range(Max_Count):
+		if get_ground_type() == Ground_Types[0]:
+			if can_harvest():
+				return harvest()
+		elif get_ground_type() != Ground_Types[1]:
+			return till()
+			
+def Plant_Entity():
+	if get_pos_x() % 2 == 0:
+		if get_entity_type() != Entitie_Types[0]:
+			return plant(Entitie_Types[0])
+	else:
+		if get_entity_type() != Entitie_Types[1]:
+			return plant(Entitie_Types[1])
 
+# TODO: Need to Harvest feature logic refactor
 def Grass_Harvest():
-	if get_ground_type() == Grounds_Types[0]:
+	if get_ground_type() == Ground_Types[0]:
 		if can_harvest():
 			harvest()
-	elif get_ground_type() != Grounds_Types[0]:
+	elif get_ground_type() != Ground_Types[0]:
 		till()
-		# plant(Entities_Types[0])
 		if can_harvest():
 			harvest()
 
 def Bush_Harvest():
-	if get_entity_type() == Entities_Types[0]:
+	if get_entity_type() == Entitie_Types[0]:
 		if can_harvest():
 			harvest()
-	elif get_entity_type() != Entities_Types[1]:
-		plant(Entities_Types[1])
+	elif get_entity_type() != Entitie_Types[1]:
+		plant(Entitie_Types[1])
 		if can_harvest():
 			harvest()
 
 def Carrot_Harvest():
-	if get_ground_type() == Grounds_Types[1]:
-		plant(Entities_Types[2])
+	if get_ground_type() == Ground_Types[1]:
+		plant(Entitie_Types[2])
 		if can_harvest():
 			harvest()
-	elif get_ground_type() != Grounds_Types[1]:
+	elif get_ground_type() != Ground_Types[1]:
 		till()
-		plant(Entities_Types[2])
+		plant(Entitie_Types[2])
 		if can_harvest():
 			harvest()
 
 # Loop Logics
 while True:
-	for _ in Location_Range(Max_Location):
+	for _ in Location_Range(Max_Count):
 		Grass_Harvest()
-		Move_Direction(Directions[0])
+		Move_Direction(Directions[Default_Count])
 	Move_Direction(Directions[1])
 
-	for _ in Location_Range(Max_Location):
+	for _ in Location_Range(Max_Count):
 		Bush_Harvest()
-		Move_Direction(Directions[0])
+		Move_Direction(Directions[Default_Count])
 	Move_Direction(Directions[1])
 
-	for _ in Location_Range(Max_Location):
+	for _ in Location_Range(Max_Count):
 		Carrot_Harvest()
-		Move_Direction(Directions[0])
+		Move_Direction(Directions[Default_Count])
 	Move_Direction(Directions[1])
